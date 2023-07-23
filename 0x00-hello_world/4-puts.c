8876d4ccbd8e73fa8cf6a711e9ed75bf93cdf0ab
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2023-07-23 23:04:39 +0100

    Add betty coding styles

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
 int main(void)
-{ puts("\"Programming is like building a multilingual puzzle");
-return 0; }
+{/*Using the puts function to print the message*/
+puts("\"Programming is like building a multilingual puzzle");
+(return 0); }
