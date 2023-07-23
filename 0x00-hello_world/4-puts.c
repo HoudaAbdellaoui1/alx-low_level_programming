0af3a1a6324c11288cb1170526ccdbc1fd6c106a
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2023-07-23 22:53:57 +0100

    add double-quotes and remove comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 int main() {
-    // Using the puts function to print the message
-    puts("Programming is like building a multilingual puzzle.");
-
-    return 0; // End the program with the value 0
+    puts("\"Programming is like building a multilingual puzzle.");
+    return 0; 
 }
