07acd8d1939e2fbb289f5357e78ec4f8b77699cc
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2023-07-24 01:13:13 +0100

    Display types sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,13 +3,13 @@
 * main - prints the size of various types on the computer 
 * Return - 0 
 */ 
-int main(void) {
-printf("Size of char: %zu byte(s)\n", sizeof(char));
-printf("Size of int: %zu byte(s)\n", sizeof(int));
-printf("Size of float: %zu byte(s)\n", sizeof(float));
-printf("Size of double: %zu byte(s)\n", sizeof(double));
-printf("Size of long int: %zu byte(s)\n", sizeof(long int));
-printf("Size of long double: %zu byte(s)\n", sizeof(long double));
-
+int main(void)
+{
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
+printf("Size of a double: %zu byte(s)\n", sizeof(double));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long double: %zu byte(s)\n", sizeof(long double));
 return (0);
 }
