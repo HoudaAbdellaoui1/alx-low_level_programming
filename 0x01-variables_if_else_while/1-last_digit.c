593f95403b22886f33d5392ec30f63084c47b931
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2023-07-30 01:25:04 +0100

    Add coding norms

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ n = rand() - RAND_MAX / 2;
 last_digit = abs(n % 10);
 
 printf("Last digit of %d is %d ", n, last_digit);
-if (last_digit> 5)
+if (last_digit > 5)
 {
 printf("and is greater than 5\n");
 }
@@ -27,7 +27,7 @@ else if (last_digit == 0)
 {
 printf("and is 0\n");
 }
-else if(last_digit < 6 && last_digit > 0)
+else if (last_digit < 6 && last_digit > 0)
 {
 printf("and is less than 6 and not 0\n");
 }
