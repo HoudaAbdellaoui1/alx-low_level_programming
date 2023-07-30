11bbc68b95259b167cfdeb423cf71bed2b2075b2
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2023-07-30 13:52:25 +0100

    Print the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,17 +10,17 @@ void times_table(void)
 {
 int i;
 int j;
-for (i = 0; i<= 9; i++)
+for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
 if (j < 9)
 {
-printf("%2d, ",i * j);
+printf("%2d, ", i * j);
 }
 else
 {
-printf("%2d",i * j);
+printf("%2d", i * j);
 }
 }
 printf("\n");
