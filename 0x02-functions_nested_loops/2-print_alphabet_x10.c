18fba53d4b022789b84fc1e07f1ac44d4df3a5b0
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2023-07-30 12:38:55 +0100

    Add coding styles + description

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet - print lower case alphabets (10 times) followed by a new line
+ * print_alphabet_x10 - print lower case alphabets (10 times) 
+ * followed by a new line
  * Return: 0 if exited properly, non-zero otherwise
  */
 
