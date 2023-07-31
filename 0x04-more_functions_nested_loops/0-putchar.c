5990ad4c70b0e3885f599efc40b5c579ee22b8a8
Houda Abdellaoui <abdellaouihouda@gmail.com>
2023-07-31 16:42:07 +0100

    Print numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main - print putchar followed by a new line
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
