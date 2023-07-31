50c2c6f60936ebf1deb263499d5c071476106d9f
Houda Abdellaoui <abdellaouihouda@gmail.com>
2023-07-31 10:47:08 +0100

    Calculate sum of 2 integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds to integers
+ * @int, @int 
+ * Return integer
+ */
+
+int add(int a, int b)
+{
+int sum;
+
+sum = a + b;
+return(sum);
+}
