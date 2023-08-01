55f542dfbfbbe53d6edceac47642732e4e885f3a
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2023-08-01 23:46:40 +0100

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - prints a square, followed by a new line,
+ * 
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
+void main ()
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+printf("FizzBuzz ");
+else if (i % 3 == 0)
+printf("Fizz ");
+else if (i % 5 == 0)
+printf("Buzz ");
+else
+printf("%d ", i);
+}
+printf("\n");
+return (0);
+}
