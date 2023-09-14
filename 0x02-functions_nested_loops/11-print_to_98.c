ab0532b500479f830eff73fef262299270e38fd3
Houda Abdellaoui <abdellaouihouda@gmail.com>
2023-09-14 11:24:24 +0100

    Minor changes

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,13 +10,26 @@
 void print_to_98(int n)
 {
 int i;
-
-for (i=n; i <= 98; i++)
+if (n <= 98) 
 {
-while (i != 98)
+for (i = n; i <= 98; i++) 
 {
-printf("%d, ", i);
-}
 printf("%d", i);
+if (i != 98)
+printf(", ");
+else 
+printf("\n");
+}
+}
+else
+{
+for (i = n; i >= 98; i--)
+{
+printf("%d", i);
+if (i != 98)
+printf(", ");
+else
+printf("\n");
+}
 }
 }
