dc9b8daa1954fa72e33bef63f845777edab5930b
Houda Abdellaoui <abdellaouihouda@gmail.com>
2023-09-21 20:49:34 +0100

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,20 +8,17 @@
  */
 
 char *_strcat(char *dest, char *src) {
-// Find the end of the dest string
 char *ptr = dest;
 while (*ptr != '\0')
 {
 ptr++;
 }
-// Copy characters from src to dest
 while (*src != '\0')
 {
 *ptr = *src;
 ptr++;
 src++;
 }
-// Null-terminate the concatenated string
 *ptr = '\0';
 return dest;
 }
