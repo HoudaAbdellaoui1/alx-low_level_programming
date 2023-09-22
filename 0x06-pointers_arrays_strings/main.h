8b36c9e1e35b0be462f0856bf5bfa3712fee6382
Houda Abdellaoui <abdellaouihouda@gmail.com>
2023-09-22 14:15:25 +0100

    Include function in main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, const char *src, int n);
 char *_strncpy(char *dest, const char *src, int n);
+int _strcmp(const char *s1, const char *s2);
