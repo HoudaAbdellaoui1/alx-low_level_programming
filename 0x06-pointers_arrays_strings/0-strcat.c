466945b6581cacdd14a4096c8299f588b0d1bb71
Houda Abdellaoui <abdellaouihouda@gmail.com>
2023-09-22 13:14:01 +0100

    Fix function description

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * _strcat - concatenate two strings,
- * @dest : string parameter
- * @src : string parameter
+ * _strcat - concatenate two strings
+ * 
+ * @dest : pointer to destination input
+ * @src :pointer to source input
+ * 
  * Return: string
  */
 
