58d4499d26b13b7f76dc0a26a0988d383d115675
Houda Abdellaoui <abdellaouihouda@gmail.com>
2023-09-22 14:25:33 +0100

    Change lowercase to uppercase

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,7 +9,8 @@
  * Return: A pointer to the resulting string dest.
  */
 
-char *_strncpy(char *dest, const char *src, int n) {
+char *_strncpy(char *dest, const char *src, int n)
+{
 int i;
 
 for (i = 0; i < n && src[i] != '\0'; i++)
