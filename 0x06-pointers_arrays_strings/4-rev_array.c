0c54a3e656a459522d99e751d0d09f7fcb1fbb18
Houda Abdellaoui <abdellaouihouda@gmail.com>
2023-09-22 14:30:22 +0100

    Change lowercase to uppercase

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,10 +10,10 @@ void reverse_array(int *a, int n)
 {
 int start = 0;
 int end = n - 1;
-int temp = a[start];
 
 while (start < end)
 {
+int temp = a[start];
 a[start] = a[end];
 a[end] = temp;
 start++;
