bc870d0688d4cb1000058079b936e5e73d3909ae
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2023-10-11 00:12:13 +0100

    Print variable

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,13 +6,21 @@
  * print_dog - Prints the attributes of a struct dog
  * @d: Pointer to the struct dog to print
  * 
+ * Return: void
  */
 
 void print_dog(struct dog *d)
 {
-if (d == NULL)
-return;
-printf("Name: %s\n", d->name ? d->name : "(nil)");
-printf("Age: %f \n", d->age);
-printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+if (d != NULL)
+{
+if (d->name != NULL)
+printf("Name: %s\n", d->name);
+else
+printf("Name: (nil)\n");
+printf("Age: %.6f\n", d->age);
+if (d->owner != NULL)
+printf("Owner: %s\n", d->owner);
+else
+printf("Owner: (nil)\n");
+}
 }
