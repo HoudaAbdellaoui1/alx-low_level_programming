81ffbf10b6ac485280d4c0b357298a3089cbcfaf
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2023-10-11 00:09:20 +0100

    Initialize a variable

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - Prints the attributes of a struct dog
+ * @d: Pointer to the struct dog to print
+ * 
+ */
+
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+return;
+printf("Name: %s\n", d->name ? d->name : "(nil)");
+printf("Age: %.2f years\n", d->age);
+printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+}
