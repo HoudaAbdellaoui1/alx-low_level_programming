cabd7468adfdd210a08490039d3130811c7f4858
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2023-10-11 00:19:15 +0100

    Create new variable

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,12 +3,20 @@
 #include <string.h>
 #include "dog.h"
 
+/**
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: pointer to new dog
+ */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *newDog = malloc(sizeof(dog_t));
 if (newDog == NULL)
-return NULL;
-
+return (NULL);
 newDog->name = strdup(name);
 newDog->age = age;
 newDog->owner = strdup(owner);
@@ -17,7 +25,7 @@ if (newDog->name == NULL || newDog->owner == NULL)
 free(newDog->name);
 free(newDog->owner);
 free(newDog);
-return NULL;
+return (NULL);
 }
 return (newDog);
 }
