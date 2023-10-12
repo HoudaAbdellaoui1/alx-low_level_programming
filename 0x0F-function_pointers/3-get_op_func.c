93750d6ae1c2487a27259e2b3b0cde70703deeb5
Houda Abdellaoui <abdellaouihouda@gmail.com>
2023-10-12 19:16:33 +0100

    Simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,24 +1,32 @@
 #include "3-calc.h"
+#include <stddef.h>
 
 /**
- * get_op_func - Selects the correct function to perform the operation.
- * @s: The operator passed as an argument to the program
- * Return: A pointer to the corresponding function
- */
+ * get_op_func - get ops function pointer of type char array
+ *               that accepts two inputs of int data type
+ *
+ * @s: a character pointer pointing to a symbol from the program argument
+ *
+ * Return: one of the operator functions to perform calculations
+*/
 
 int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
-{"+", op_add},{"-", op_sub},{"*", op_mul},
-{"/", op_div}, {"%", op_mod}, {NULL, NULL}};
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
 int i = 0;
 
-while (ops[i].op)
+while (i < 5)
 {
-if (*(ops[i].op) == *s && s[1] == '\0')
+if (*s == *ops[i].op)
 return (ops[i].f);
 i++;
 }
-printf("Error\n");
-exit(99);
+return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,22 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
+#include <stdio.h>
+#include <string.h> /*for atoi*/
+#include <stdlib.h> /*for exit*/
 
+/**
+ * main - Program Entry
+ *
+ * Description: Program that takes 4 arguments where 2 are
+ *              int variables plus an operator to perform
+ *              a function between the two numbers
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0-on success
+*/
 int main(int argc, char *argv[])
 {
+int num1, num2;
+char *operator;
 if (argc != 4)
 {
 printf("Error\n");
-return (98);
+exit(98);
 }
-int num1 = atoi(argv[1]);
-int num2 = atoi(argv[3]);
-char *operator = argv[2];
-
-    int (*func)(int, int) = get_op_func(operator);
-
-    int result = func(num1, num2);
-    printf("%d\n", result);
-
-    return (0);
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]); 
+operator = argv[2];
+if (get_op_func(operator) == NULL || operator[1] != '\0')
+{
+printf("Error\n");
+exit(99);
+}
+if ((*operator == 47 || *operator == 37) && num2 == 0)
+{
+printf("Error\n");
+exit(100);
+}
+printf("%d\n", get_op_func(operator)(num1, num2));
+return (0);
 }
