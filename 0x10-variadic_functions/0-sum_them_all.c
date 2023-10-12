e7614ebf4b4bae566745c7be7482e95a9d29065e
Houda Abdellaoui <abdellaouihouda@gmail.com>
2023-10-12 17:54:08 +0100

    Calculate sum of parameters

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * *sum_them_all - sum parameters
+ * @n: the number of parameters
+ * @...: variable number of arguments
+ * Return: the sum of all parameters
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+va_list args;
+int sum = 0, num;
+unsigned int i = 0;
+    
+va_start(args, n);
+if (n == 0)
+{
+va_end(args);
+return 0;
+}
+for (i = 0; i < n; i++)
+{
+num = va_arg(args, int);
+sum += num;
+}
+va_end(args);
+return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,9 @@
+# ALX 2023 Software Engineering - C- malloc, free
+
+Welcome to the variadic functions, free repository for the ALX Software Engineering Program 2023! This repository is designed to provide a comprehensive understanding of variadic functions and how to use them in the C programming language.
+
+# What are variadic functions ?
+# How to use va_start, va_arg and va_end macros ?
+# Why and how to use the const type qualifier ?
+
+Happy coding! 🚀
