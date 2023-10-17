804dfde65fc3d9c53337cc80ffb4ab28a79f3d22
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2023-10-17 23:49:53 +0100

    Print function name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 if (argc > 0)
 {
 char *program_name = argv[0];
 char *last_slash = strrchr(program_name, '/');
 
 if (last_slash != NULL)
-program_name = last_slash + 1;
+program_name = last_slash + 1;       
+if (strncmp(program_name, "./", 2) == 0)
+program_name += 2;        
 printf("%s\n", program_name);
-}
+}    
 return (0);
 }
