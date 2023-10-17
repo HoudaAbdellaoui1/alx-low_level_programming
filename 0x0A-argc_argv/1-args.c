6387f641c5b3b39d5879f7853ed9d80f3aa44744
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2023-10-17 23:56:09 +0100

    Print number of arguemnts passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of arguments
+ * @argv: An array containing the arguments
+ *
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+int num_arguments = argc - 1;
+
+printf("%d\n", num_arguments);
+return (0);
+}
