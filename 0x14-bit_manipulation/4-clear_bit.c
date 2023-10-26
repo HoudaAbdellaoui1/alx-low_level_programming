fc4af7cbee4aca513e80d090c202db6aaa011da4
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2023-10-26 23:31:32 +0100

    Set value of a bit to 0 in a given index'

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,7 +7,7 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
     if (index >= sizeof(unsigned long int) * 8)
-        return -1; // Error: Index is out of range.
+        return -1;
 
     *n = *n & ~(1UL << index);
     return 1;
