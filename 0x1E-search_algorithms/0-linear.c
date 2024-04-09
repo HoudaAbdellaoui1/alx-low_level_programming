078539d5e53c6c7411905d3a3be6bf599f6ee73b
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-04-09 00:26:40 +0000

    test

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,7 +15,7 @@ int linear_search(int *array, size_t size, int value)
 
     if (array == NULL)
     {
-        return -1;
+    return (-1);
     }
 
     for (i = 0; i < size; i++)
@@ -23,9 +23,9 @@ int linear_search(int *array, size_t size, int value)
         printf("Value checked array[%u] = [%d]\n", (unsigned int)i, array[i]);
         if (array[i] == value)
         {
-            return i;
+            return (i);
         }
     }
     printf("Value checked array[%u] = [%d]\n", ((unsigned int)size), array[size]);
-    return -1;
+    return (-1);
 }
