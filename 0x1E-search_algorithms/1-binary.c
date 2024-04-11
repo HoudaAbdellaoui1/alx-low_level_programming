7196fa146b9f1c9bf1dbccb3308a1f7a11e073e4
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-04-11 02:25:21 +0000

    Binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,42 +13,42 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-    size_t l = 0;
-    size_t r = size - 1;
-    size_t i;
+size_t l = 0;
+size_t r = size - 1;
+size_t i;
 
-    if (array == NULL || size == 0)
-    {
-        return (-1);
-    }
-    else
-    {
-        while (l <= r)
-        {
-            size_t m = (l + r) / 2;
-            printf("Searching in array: ");
-            for (i = l; i <= r; i++)
-            {
-                printf("%d", array[i]);
-                if (i < r)
-                {
-                    printf(", ");
-                }
-            }
-            printf("\n");
-            if (array[m] < value)
-            {
-                l = m + 1;
-            }
-            else if (array[m] > value)
-            {
-                r = m - 1;
-            }
-            else if (array[m] == value)
-            {
-                return (m);
-            }
-        }
-        return (-1);
-    }
+if (array == NULL || size == 0)
+{
+return (-1);
+}
+else
+{
+while (l <= r)
+{
+size_t m = (l + r) / 2;
+printf("Searching in array: ");
+for (i = l; i <= r; i++)
+{
+printf("%d", array[i]);
+if (i < r)
+{
+    printf(", ");
+}
+}
+printf("\n");
+if (array[m] < value)
+{
+l = m + 1;
+}
+else if (array[m] > value)
+{
+r = m - 1;
+}
+else if (array[m] == value)
+{
+return (m);
+}
+}
+return (-1);
+}
 }
