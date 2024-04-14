89c73f3de2274f2e8d3e35b5e2d0c14047f814e4
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-04-14 00:52:15 +0000

    betty fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -19,8 +19,9 @@ for (i = 0; i < size; i++)
 {
 printf("Value checked array[%u] = [%d]\n", (unsigned int)i, array[i]);
 if (array[i] == value)
+{
 return (i);
 }
-printf("Value checked array[%u] = [%d]\n", ((unsigned int)size), array[size]);
+}
 return (-1);
 }
