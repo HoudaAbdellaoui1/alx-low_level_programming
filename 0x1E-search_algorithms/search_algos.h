778d9b11bd29d0270d55d79d8a8296e0f5093bbd
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-04-14 00:57:00 +0000

    Search algos

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,4 +1,9 @@
-#include <stddef.h>
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H */
+
